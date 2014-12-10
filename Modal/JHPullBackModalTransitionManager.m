cbdbec4d1b5a3e182d0e0dc5e4ed15bca95daed3
Jeff Hurray <jhurray@0587356876.wireless.umich.net>
2014-12-10 17:46:26 -0500

    Fixed bugs

--- Modal/JHPullBackModalTransitionManager.m
+++ Modal/JHPullBackModalTransitionManager.m
@@ -15,23 +15,21 @@
 @implementation JHPullBackModalTransitionManager
 
 -(void)animatePresentTransitionFrom:(UIViewController *)fromVC to:(UIViewController *)toVC withTransitionContext:(id<UIViewControllerContextTransitioning>)transitionContext {
-
+    
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     UIView *containerView = [transitionContext containerView];
     CGRect toEndFrame  = toVC.view.frame;
     CGRect toStartFrame = CGRectMake( 0, screenBounds.size.height, screenBounds.size.width, screenBounds.size.height);
-    [toVC.view setFrame:toStartFrame];
     [containerView insertSubview:toVC.view aboveSubview:fromVC.view];
+    [toVC.view setFrame:toStartFrame];
     CGAffineTransform shrink = CGAffineTransformMakeScale(0.95, 0.95);
     CGAffineTransform shiftDown = CGAffineTransformMakeTranslation(0, 20);
     CGAffineTransform shrinkAndShift = CGAffineTransformConcat(shrink, shiftDown);
-    [UIView animateKeyframesWithDuration:[self transitionDuration:transitionContext] delay:0.0 options:0 animations:^{
-        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.3 animations:^{
-            fromVC.view.transform = shrinkAndShift;
-        }];
-        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:[self transitionDuration:transitionContext] animations:^{
-            [toVC.view setFrame:toEndFrame];
-        }];
+    [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        fromVC.view.transform = shrinkAndShift;
+    } completion:nil];
+    [UIView animateWithDuration:0.3 delay:0.1 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        [toVC.view setFrame:toEndFrame];
     } completion:^(BOOL finished) {
         [transitionContext completeTransition:YES];
     }];
@@ -49,16 +47,13 @@
     
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     CGRect toEndFrame = CGRectMake(0, screenBounds.size.height, screenBounds.size.width, screenBounds.size.height);
-    
     CGAffineTransform grow = CGAffineTransformMakeScale(1.0, 1.0);
     
-    [UIView animateKeyframesWithDuration:[self transitionDuration:transitionContext] delay:0.0 options:0 animations:^{
-        [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:[self transitionDuration:transitionContext] animations:^{
-            [modalSnapshot setFrame:toEndFrame];
-        }];
-        [UIView addKeyframeWithRelativeStartTime:0.3   relativeDuration:0.3 animations:^{
-            toVC.view.transform = grow;
-        }];
+    [UIView animateWithDuration:0.2 delay:0.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [modalSnapshot setFrame:toEndFrame];
+    } completion:nil];
+    [UIView animateWithDuration:0.3 delay:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        toVC.view.transform = grow;
     } completion:^(BOOL finished) {
         [transitionContext completeTransition:YES];
     }];
--- Navigation/JHZoomNavigationTransitionManager.m
+++ Navigation/JHZoomNavigationTransitionManager.m
@@ -19,15 +19,14 @@
 
 -(void)animatePresentTransitionFrom:(UIViewController *)fromVC to:(UIViewController *)toVC withTransitionContext:(id<UIViewControllerContextTransitioning>)transitionContext {
     UIView *containerView = [transitionContext containerView];
+    [containerView insertSubview:toVC.view aboveSubview:fromVC.view];
     [toVC.view setAlpha:0.0];
     toVC.view.transform = CGAffineTransformMakeScale(2.0, 2.0);
-    [containerView insertSubview:toVC.view aboveSubview:fromVC.view]; 
-    
     [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
         toVC.view.transform = CGAffineTransformMakeScale(1.0, 1.0);
         [toVC.view setAlpha:1.0];
     } completion:^(BOOL finished) {
-        [transitionContext completeTransition:YES]; 
+        [transitionContext completeTransition:YES];
     }];
 }
 
@@ -38,6 +37,7 @@
         fromVC.view.transform = CGAffineTransformMakeScale(2.0, 2.0);
         [fromVC.view setAlpha:0.0];
     } completion:^(BOOL finished) {
+        fromVC.view.transform = CGAffineTransformMakeScale(1.0, 1.0);
         [fromVC.view removeFromSuperview];
         [transitionContext completeTransition:YES];
     }];
